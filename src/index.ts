61ab8664482ca374f6f2366d66874d41bba26d2e
ahmed <ahmedismailm85@gmail.com>
2022-02-14 08:58:03 +0200

    feat: format

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // NOTE: This code has not been converted to TypeScript yet
-import arrays from './utilities/arrays.js';
+import arrays from './utilities/arrays.js'
 import numbers from './utilities/numbers.js'
 import strings from './utilities/strings.js'
 
@@ -7,8 +7,8 @@ const numArr: number[] = [3, 4, 5, 6]
 const wordArr: string[] = ['cat', 'dog', 'rabbit', 'bird']
 const arrSum = arrays.addArr(numArr)
 const mixArr = arrays.concatArr(numArr, wordArr)
-const myNum = ("15" as unknown) as number % 2
-const five =   parseInt('5')
+const myNum = ('15' as unknown) as number % 2
+const five = parseInt('5')
 // results of function calls
 console.log(arrays.cut3(mixArr))
 console.log(numbers.sum(arrSum, myNum))
