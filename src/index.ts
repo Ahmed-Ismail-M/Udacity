265bb270a80f2570c15b998625f658d2bbe7055f
ahmed <ahmedismailm85@gmail.com>
2022-02-16 11:06:41 +0200

    feat: init express server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import axios from 'axios';
-import arrays from './utilities/arrays'
 
-console.log('egy')
+import Express from 'express';
+
+const app = Express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('CimaLight.mp4')
+  console.log(`A message from ${req.ip} : ${req.query.msg}`)
+
+})
+
+app.listen(port, () => { console.log(`server started at http://localhost:${port}`) })
