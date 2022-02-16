efa819959a6d0ca4cb1e6657ab31530923f9b814
ahmed <ahmedismailm85@gmail.com>
2022-02-16 06:59:25 +0200

    feat: test functions

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,6 +1,19 @@
 
-import myfunc from '../index'
+import strings from '../utilities/strings'
+import numbers from '../utilities/numbers'
+import arrays from '../utilities/arrays'
 
-it('expect myfunc() to equal hello', () => {
-  expect(myfunc()).toEqual('Hello');
+const numbArr = [1, 5, 7]
+const wordArr = ['ahmed', 'mohmed', 'aly']
+it('expect strings.cap() to equal hello', () => {
+  expect(strings.capitalize('Ahmed')).toEqual('Ahmed');
+});
+it('expect numbers() to equal hello', () => {
+  expect(numbers.multiply(10, 4)).toBeCloseTo(40);
+});
+it('should not contain the third index', () => {
+  expect(arrays.cut3(wordArr)).not.toContain('aly');
+});
+it('should not contain the third index', () => {
+  expect(arrays.addArr(numbArr)).toBeTruthy();
 });
