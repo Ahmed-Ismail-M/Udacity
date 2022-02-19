d677d759f1736b4236ab7c38cfbfbf70774b097d
ahmed <ahmedismailm85@gmail.com>
2022-02-19 09:46:06 +0200

    feat: testing server with wrong inputs

--- src/tests/utilities/resizeSpec.ts
+++ src/tests/utilities/resizeSpec.ts
@@ -32,9 +32,33 @@ describe('Test Server', () => {
   describe('test /api/resize with wrong parameters', () => {
     it('shoud refuse paramters because one is missing', (done) => {
       request(app)
-        .get('/api/resize?filename=assets.jpg&height=200')
+        .get('/api/resize?filename=encenadaport.jpg&height=200')
         .expect(500)
         .expect('Post image info with parameters filename,height,width')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .end((error:Error) => (error) ? done.fail(error) : done());
+    });
+    it('shoud return error width should be a number', (done) => {
+      request(app)
+        .get('/api/resize?filename=encenadaport.jpg&height=200&width=string')
+        .expect(500)
+        .expect('Error: Expected positive integer for width but received NaN of type number')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .end((error:Error) => (error) ? done.fail(error) : done());
+    });
+    it('shoud return error height should be a number', (done) => {
+      request(app)
+        .get('/api/resize?filename=encenadaport.jpg&height=string&width=200')
+        .expect(500)
+        .expect('Error: Expected positive integer for height but received NaN of type number')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .end((error:Error) => (error) ? done.fail(error) : done());
+    });
+    it('shoud return error invalid input', (done) => {
+      request(app)
+        .get('/api/resize?filename=wornginput&height=200&width=200')
+        .expect(500)
+        .expect('Error: Input file is missing')
         .expect('Content-Type', 'text/html; charset=utf-8')
         .end((error:Error) => (error) ? done.fail(error) : done());
     });
